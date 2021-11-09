5b308d64843afc4033f599cdb2a5ba18e9529456
LightDestory <apb231@gmail.com>
2021-11-09 19:13:35 +0100

    Pyrogram 4 the win

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.9.7-alpine
 
 WORKDIR /app
 COPY tg_downloader.py /app/tg_downloader.py
-RUN apk update && apk upgrade && apk add build-base && pip install telethon cryptg
+RUN apk update && apk upgrade && apk add build-base && pip install pyrogram tgcrypto
 RUN chmod +x tg_downloader.py
 
 ENTRYPOINT ["python"]
--- tg_downloader.py
+++ tg_downloader.py
@@ -1,19 +1,112 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 import os
 import sys
 import time
 import asyncio
 
+from asyncio import Task
 # Import the client
-from telethon import TelegramClient, events
-from telethon.tl import types
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.handlers import MessageHandler
 
-# Enable logging
-import logging
+
+class Downloader:
+
+    def __init__(self, session: str, api_id: int, api_hash: str, bot_token: str, download_path: str,
+                 parallel_downloads: int,
+                 download_timeout: int) -> None:
+        self._session = session
+        self._api_id = api_id
+        self._api_hash = api_hash
+        self._bot_token = bot_token
+        self._download_path = download_path
+        self._parallel_downloads = parallel_downloads
+        self._download_timeout = download_timeout
+        self._create_workflow()
+
+    def _clean_up(self) -> None:
+        for task in self._tasks:
+            task.cancel()
+
+    def _create_workflow(self) -> None:
+        self._queue = asyncio.Queue()
+        self._tasks: list[Task] = []
+        self._client = Client(self._session, self._api_id, self._api_hash, bot_token=self._bot_token)
+        # Create worker tasks to process the queue concurrently.
+        for i in range(self._parallel_downloads):
+            loop = asyncio.get_event_loop()
+            task = loop.create_task(self._worker(f'worker-{i}'))
+            self._tasks.append(task)
+
+    def _update_progress(self, current: int, total: int, *args) -> None:
+        reply: Message = args[0]
+        try:
+            reply.edit(f"{current * 100 / total:.1f}%")
+        except:
+            pass
+
+    async def _worker(self, name):
+        while True:
+            # Get a "work item" out of the queue.
+            queue_item = await self._queue.get()
+            message: Message = queue_item[0]
+            reply: Message = queue_item[1]
+            file_name = message.document.file_name
+            file_path = os.path.join(self._download_path, file_name)
+            await reply.edit('Downloading...')
+            print("[%s] Download started at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+            try:
+                loop = asyncio.get_event_loop()
+                task = loop.create_task(self._client.download_media(message, file_path, progress=self._update_progress,
+                                                                    progress_args=[reply]))
+                await asyncio.wait_for(task, timeout=self._download_timeout)
+                end_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                end_time_short = time.strftime("%H:%M", time.localtime())
+                print("[%s] Successfully downloaded at %s" % (file_name, end_time))
+                await reply.edit('Finished at %s' % end_time_short)
+            except asyncio.TimeoutError:
+                print("[%s] Timeout reached at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+                await reply.edit('Error!')
+                await message.reply_text('ERROR: Timeout reached downloading this file', quote=True)
+            except Exception as e:
+                print("[EXCEPTION]: %s" % (str(e)))
+                # print("[%s]: %s" % (e.__class__.__name__, str(e)))
+                print("[%s] Exception at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+                await reply.edit('Error!')
+                await message.reply_text(
+                    'ERROR: Exception %s raised downloading this file: %s' % (e.__class__.__name__, str(e)), quote=True)
+
+            # Notify the queue that the "work item" has been processed.
+            self._queue.task_done()
+
+    # This is our update handler. It is called when a new update arrives.
+    async def _handler(self, client: Client, message: Message):
+        if message.media is not None:
+            file_name = message.document.file_name
+            print("[%s] Download queued at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+            reply = await message.reply_text('In queue', quote=True)
+            await self._queue.put([message, reply])
+
+    def run(self) -> None:
+        try:
+            handler = MessageHandler(self._handler, filters.private)
+            # Register the update handler so that it gets called
+            self._client.add_handler(handler)
+            # Run the client until Ctrl+C is pressed, or the client disconnects
+            print('Successfully started (Press Ctrl+C to stop)')
+            self._client.run()
+        finally:
+            # Cancel our worker tasks.
+            self._clean_up()
+            # Stop Telethon client
+            print('Stopped!')
+
 
 # This is a helper method to access environment variables or
 # prompt the user to type them in the terminal if missing.
-def get_env(name, message, cast=str):
+def get_env(name: str, message: str, cast=str) -> str | int:
     if name in os.environ:
         return os.environ[name]
     while True:
@@ -24,111 +117,24 @@ def get_env(name, message, cast=str):
             print(e, file=sys.stderr)
             time.sleep(1)
 
-# Define some variables so the code reads easier
-session = os.environ.get('TG_SESSION', 'tg_downloader')
-api_id = get_env('TG_API_ID', 'Enter your API ID: ', int)
-api_hash = get_env('TG_API_HASH', 'Enter your API hash: ')
-bot_token = get_env('TG_BOT_TOKEN', 'Enter your Telegram BOT token: ')
-download_path = get_env('TG_DOWNLOAD_PATH', 'Enter full path to downloads directory: ')
-debug_enabled = ('DEBUG_ENABLED' in os.environ)
-if(debug_enabled):
-    logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.DEBUG)
-else:
-    logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.ERROR)
 
-number_of_parallel_downloads = int(os.environ.get('TG_MAX_PARALLEL',4))
-maximum_seconds_per_download = int(os.environ.get('TG_DL_TIMEOUT',3600))
-proxy = None  # https://github.com/Anorov/PySocks
+# Get information needed to run the bot
+def init_settings() -> []:
+    session = os.environ.get('TG_SESSION', 'tg_downloader')
+    api_id = get_env('TG_API_ID', 'Enter your API ID: ', int)
+    api_hash = get_env('TG_API_HASH', 'Enter your API hash: ')
+    bot_token = get_env('TG_BOT_TOKEN', 'Enter your Telegram BOT token: ')
+    download_path = get_env('TG_DOWNLOAD_PATH', 'Enter full path to downloads directory: ')
+    parallel_downloads = int(os.environ.get('TG_MAX_PARALLEL', 4))
+    download_timeout = int(os.environ.get('TG_DL_TIMEOUT', 5400))
+    return session, api_id, api_hash, bot_token, download_path, parallel_downloads, download_timeout
 
-#Create a queue that we will use to store our downloads.
-queue = asyncio.Queue()
 
-#Create tmp path to store downloads until completed
-tmp_path = os.path.join(download_path,"tmp")
-os.makedirs(tmp_path, exist_ok=True)
+def main() -> None:
+    # Create instance of Downloader
+    downloader = Downloader(*init_settings())
+    downloader.run()
 
-async def worker(name):
-    while True:
-        # Get a "work item" out of the queue.
-        queue_item = await queue.get()
-        update = queue_item[0]
-        message = queue_item[1]
 
-        file_path = tmp_path;
-        file_name = 'unknown name';
-        attributes = update.message.media.document.attributes
-        for attr in attributes:
-            if isinstance(attr, types.DocumentAttributeFilename):
-                file_name = attr.file_name
-                file_path = os.path.join(file_path, attr.file_name)
-        await message.edit('Downloading...')
-        print("[%s] Download started at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        try:
-            loop = asyncio.get_event_loop()
-            task = loop.create_task(client.download_media(update.message, file_path))
-            download_result = await asyncio.wait_for(task, timeout=maximum_seconds_per_download)
-            end_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            end_time_short = time.strftime("%H:%M", time.localtime())
-            # os.chown(download_result, int(user_id), int(group_id))
-            _,filename = os.path.split(download_result)
-            final_path = os.path.join(download_path,filename)
-            os.rename(download_result,final_path)
-            print("[%s] Successfully downloaded to %s at %s" % (file_name, final_path, end_time))
-            await message.edit('Finished at %s' %(end_time_short))
-        except asyncio.TimeoutError:
-            print("[%s] Timeout reached at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-            await message.edit('Error!')
-            message = await update.reply('ERROR: Timeout reached downloading this file')
-        except Exception as e:
-            print("[EXCEPTION]: %s" % (str(e)))
-            #print("[%s]: %s" % (e.__class__.__name__, str(e)))
-            print("[%s] Exception at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-            await message.edit('Error!')
-            message = await update.reply('ERROR: Exception %s raised downloading this file: %s' % (e.__class__.__name__, str(e)))
-
-        # Notify the queue that the "work item" has been processed.
-        queue.task_done()
-
-client = TelegramClient(session, api_id, api_hash, proxy=proxy, request_retries=10, flood_sleep_threshold=120)
-
-# This is our update handler. It is called when a new update arrives.
-# Register `events.NewMessage` before defining the client.
-@events.register(events.NewMessage)
-async def handler(update):
-    if(debug_enabled):
-        print(update)
-    if update.message.media is not None:
-        file_name = 'unknown name';
-        attributes = update.message.media.document.attributes
-        for attr in attributes:
-            if isinstance(attr, types.DocumentAttributeFilename):
-                file_name = attr.file_name
-        print("[%s] Download queued at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        message = await update.reply('In queue')
-        await queue.put([update, message])
-
-try:
-    # Create worker tasks to process the queue concurrently.
-    tasks = []
-    for i in range(number_of_parallel_downloads):
-        loop = asyncio.get_event_loop()
-        task = loop.create_task(worker(f'worker-{i}'))
-        tasks.append(task)
-
-    # Start client with TG_BOT_TOKEN string
-    client.start(bot_token=str(bot_token))
-    # Register the update handler so that it gets called
-    client.add_event_handler(handler)
-
-    # Run the client until Ctrl+C is pressed, or the client disconnects
-    print('Successfully started (Press Ctrl+C to stop)')
-    client.run_until_disconnected()
-finally:
-    # Cancel our worker tasks.
-    for task in tasks:
-        task.cancel()
-    # Wait until all worker tasks are cancelled.
-    #await asyncio.gather(*tasks, return_exceptions=True)
-    # Stop Telethon client
-    client.disconnect()
-    print('Stopped!')
+if __name__ == '__main__':
+    main()
